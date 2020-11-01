45f22a29f602868abd05f80f4d12ab7a04b97b16
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-31 20:28:47 -0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class My_List(models.Model):
+    item = models.CharField(max_length=50)
+    qty = models.IntegerField()
+    subsititute = models.CharField(max_length=50, null=True)
+    urgent = models.BooleanField()
+
+class Sub_List(models.Model):
+    recipes = models.CharField(max_length=50)
+    list = models.ForeignKey(My_List, on_delete=models.CASCADE)
+
+class Store_List(models.Model):
+    store_name = models.CharField(max_length = 50)
+    lists = models.ManyToManyField(My_List)

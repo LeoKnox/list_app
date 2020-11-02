62ec9b8be6cb8ef57cd6db3d01c48067ab122d9e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-11-01 19:08:16 -0800

    Add files via upload

--- lists/urls.py
+++ lists/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index/', views.index, name='index'),
-    path('room/<int:room_id>', views.room, name='room'),
     path('create/', views.create, name='create'),
-    path('delete/<int:room_id>', views.delete, name='delete'),
-    path('edit/<int:room_id>', views.edit, name='edit'),
 ]
